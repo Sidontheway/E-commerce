d6b40ead8d359d6fd6235de6df6d4ef70823d605
Siddhartha Tiwari <73224879+Sidontheway@users.noreply.github.com>
2023-11-25 16:13:30 +0000

    Second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar/>
     </div>
   );
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
+import logo from '../Assests/logo.png'
+import cart_item from '../Assests/cart_icon.png'
+
 const Navbar = () =>{
+  const [menu , setmenu] = useState("shop");
+
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+
+      <ul className='nav-menu'>
+        <li onClick={()=>{setmenu("shop")}}>Shop {menu == "shop" ? <hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("menu")}}>Men {menu == "menu" ? <hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("women")}}>Women {menu == "women" ? <hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("kids")}}>Kids {menu == "kids" ? <hr/>:<></>}</li> 
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_item} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
   )
 }
