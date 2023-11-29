8e2b4b9926532341339d5f587aac4e93024f2221
Siddhartha Tiwari <73224879+Sidontheway@users.noreply.github.com>
2023-11-29 05:20:27 +0000

    Added

---
+++ frontend/src/Components/Items/Items.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Items.css'
+
+const Items = (props) => {
+  return (
+    <div className='item'>
+        <img src= {props.image} alt="" />
+        <p>{props.name}</p>
+
+        <div className='items_prices'>
+            <div className='price_new'>
+                ${props.new_price}
+            </div>
+
+            <div className="price_old">
+                {props.old_price}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Items;
